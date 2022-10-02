8fe868452b8e5e2585fc1c407cad08c2c374f035
TanVol <114764793+TanVol@users.noreply.github.com>
2022-10-02 14:17:22 +0000

    super

--- foodap.rb
+++ foodap.rb
@@ -1,10 +1,36 @@
 require 'sinatra'
+require "yaml/store"
+
 Choices = {
     "pizza"=> "Pizza",
     "pierogi" => "Pierogi",
-    "barszcz" => "Barszcz"
+    "barszcz" => "Barszcz",
+    "schaboszczak" => "Kotlet schabowy"
 }
 get '/' do
-    @tittle = "Co tam sobie dzisiaj zjemy?"
+    @title = "Co tam sobie dzisiaj zjemy?"
     erb :index
+end
+post '/cast' do
+  @title =  "Tutaj będzie co wybrałeś"
+  @choice = Choices[params["vote"]]
+  @vote = params["vote"]
+  @store = YAML::Store.new 'votes.yml'
+
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+  erb :cast
+end
+get '/results' do
+    @title = "Nasze wyniki"
+    @store = YAML::Store.new 'votes.yml'
+   @votes = @store.transaction do
+    @store['votes'] ||= {}
+    @store ["votes"]
+    end
+    puts @votes
+erb :results
 end
